e3a27d14923c17bfa63558bd70cbbfc5d2e03f7c
dlee831 <71424261+dlee831@users.noreply.github.com>
2020-09-16 23:55:33 -0700

    Add files via upload

--- sql queries.sql
+++ sql queries.sql
@@ -1,12 +1,16 @@
+.headers on
 .mode csv
 .import C:/sqlite3/showwcase_sessions.csv SESSIONS
+
+.output C:/sqlite3/showwcase_data.csv
 
 SELECT 
 	customer_id, 
 	SUM(session_projects_added+session_likes_given+session_comments_given) as active_engage,
-	SUM(session_duration-inactive_duration) as passive_engage,
+	SUM(session_duration) as passive_engage,
 	COUNT(DISTINCT login_date) as engage_frequency
 FROM
 	SESSIONS
 GROUP BY
-	customer_id
+	customer_id;
+
