801a6ab9915d6d80419e669bb399af6ee78b5a5c
dlee831 <71424261+dlee831@users.noreply.github.com>
2020-09-16 22:17:56 -0700

    Delete showcase.sql

--- showcase.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-	customer_id, 
-	SUM(session_projects_added+session_likes_given+session_comments_given) as active_engage,
-	SUM(session_duration-inactive_duration) as passive_engage,
-	COUNT(DISTINCT login_date) as engage_frequency
-FROM
-	SESSIONS
-GROUP BY
-	customer_id
